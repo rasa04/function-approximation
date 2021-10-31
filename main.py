007d44abd355722cdbb9200767e69398506a5934
Rasul <abdurasul2004.02@gmail.com>
2021-10-31 18:19:53 +0500

    v1.4

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
- main file
-"""
-equal = y_we_need
-for i in range(k):
-    tes = 1
-    for j in range(i + 1):
-        tes *= t - j
-    equal += tes  # *
---
+++ readme.md
@@ -0,0 +1,2 @@
+this program is created by Khujamberdiyev Abdurasul in the 31-october of 2021
+tel: +998 90 035 0295
